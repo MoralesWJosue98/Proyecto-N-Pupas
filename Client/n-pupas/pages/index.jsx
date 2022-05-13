979daa8941d8bfbeaffe10c326fb340516654773
Carolina <00008119@uca.edu.sv>
2022-05-12 23:26:27 -0600

    Remove NextJS sample files
    Remove files generated on a new NextJS project and change files
    extension to .jsx
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

---
+++ Client/n-pupas/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>N Pupas</title>
+        <meta name='description' content='Proyecto de Programación N Capas' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  );
+}
