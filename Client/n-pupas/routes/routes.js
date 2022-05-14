7ba6e052a47747c29884866885291f65a1ebc23b
carolinesierra <00091119@uca.edu.sv>
2022-05-14 01:08:50 -0500

    Add routes admin
    Add routes admin for each pages
    
    Authored by Caroline Sierra <00091119@uca.edu.sv>

--- Client/n-pupas/routes/routes.js
+++ Client/n-pupas/routes/routes.js
@@ -7,3 +7,24 @@ export const employeeRoutes = {
     report: '/empleado/informe',
     profile: '/empleado/perfil',
   };
+
+  export const adminRoutes = {
+    home: '/admin',
+    purchases: '/admin/compras',
+    employees: '/admin/empleados',
+    menu: '/admin/menu',
+    reports: '/admin/reportes',
+    branches: '/admin/sucursales',
+    sales: '/admin/ventas',
+    profile: '/admin/perfil',
+    newPurchase: '/admin/nueva-compra',
+    newProduct: '/admin/nuevo-producto',
+    newEmployee: '/admin/nuevo-empleado',
+    newBranch: '/admin/nueva-sucursal',
+    newSale: '/admin/nueva-venta',
+    newReport: '/admin/nuevo-reporte/',
+    editProduct: '/admin/modificar-producto',
+    editEmployee: '/admin/modificar-empleado',
+    editBranch: '/admin/modificar-sucursal',
+    editSale: '/admin/modificar-venta',
+  };
