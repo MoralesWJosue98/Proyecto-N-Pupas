fffedc63ab2ffb1ac4cb93038777b6676dfd6e08
carolinesierra <00091119@uca.edu.sv>
2022-05-14 00:54:55 -0500

    Add routes employess
    Add routes employess for each  pages
    
    Authored by Caroline Sierra <00091119@uca.edu.sv>

--- Client/n-pupas/routes/routes.js
+++ Client/n-pupas/routes/routes.js
@@ -1,3 +1,9 @@
 export const loginRoute = '/login';
 export const registerRoute = '/registro';
 
+export const employeeRoutes = {
+    home: '/empleado',
+    comments: '/empleado/comentarios',
+    report: '/empleado/informe',
+    profile: '/empleado/perfil',
+  };
