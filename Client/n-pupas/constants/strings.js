b52fe1a381a2207dd5b4f38fbf15c11491a4d32d
MoralesWJosue98 <wilfredo.josue@live.com>
2022-05-15 12:08:51 -0600

    Add: routes and page names for employee and admin

--- Client/n-pupas/constants/strings.js
+++ Client/n-pupas/constants/strings.js
@@ -1,3 +1,32 @@
+export const profilePageName = 'Perfil';
+export const homePageName = 'Inicio';
+export const registerPageName = 'Formulario de Registro';
+export const loginPageName = 'Inicio de Sesión';
+
+export const employeePages = {
+  comments: 'Comentarios y reportes',
+  report: 'Informe general',
+};
+
+export const adminPages = {
+  purchases: 'Compras',
+  employees: 'Empleados',
+  menu: 'Menú',
+  reports: 'Reportes',
+  branches: 'Sucursales',
+  sales: 'Ventas',
+  newPurchase: 'Registrar compra',
+  newProduct: 'Agregar producto',
+  newEmployee: 'Agregar empleado',
+  newBranch: 'Agregar sucursal',
+  newSale: 'Registrar venta',
+  newReport: 'Nuevo reporte a empleado',
+  editProduct: 'Modificar producto',
+  editEmployee: 'Modificar empleado',
+  editBranch: 'Modificar sucursal',
+  editSale: 'Modificar venta',
+};
+
 export const crudActionTypes = {
   create: 'Comentario',
   read: 'Ver',
