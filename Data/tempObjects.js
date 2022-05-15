8e3e69c910d65cfb6421477e7ef2fd43291ad1e6
MoralesWJosue98 <wilfredo.josue@live.com>
2022-05-15 12:55:18 -0600

    Add: temporal data to employee and admin

---
+++ Data/tempObjects.js
@@ -0,0 +1,20 @@
+export const testEmployee = {
+    name: 'Susana Dolores de Cabezas',
+    username: 'sussy123',
+    pupuseria: 'Pupusería la Bendición',
+    branch: 'Peatonal UCA',
+    role: 'Empleado',
+    hiring: '12/09/2020',
+    salary: 400,
+};
+
+export const testAdmin = {
+    name: 'José Antonio Cerritos',
+    username: 'jac2000',
+    pupuseria: 'Pupusería La Bendición',
+    branch: 'Peatonal UCA',
+    role: 'Admin',
+    hiring: '12/09/2020',
+    openingDate: '10/09/2019'
+};
+
