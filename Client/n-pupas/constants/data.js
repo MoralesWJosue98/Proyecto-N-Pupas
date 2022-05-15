2517aa7409f9d18050bda16e4a5f2d26de35573a
MoralesWJosue98 <wilfredo.josue@live.com>
2022-05-15 12:23:16 -0600

    Add: routes for menu options and data for departments

---
+++ Client/n-pupas/constants/data.js
@@ -0,0 +1,34 @@
+import { employeeRoutes, adminRoutes } from 'routes/routes';
+import { employeePages, adminPages } from './strings';
+
+export const employeeMenuOptions = [
+    { title: employeePages.comments, route: employeeRoutes.comments },
+    { title: employeePages.report, route: employeeRoutes.report },
+];
+
+export const adminMenuOptions = [
+    { title: adminPages.sales, route: adminRoutes.sales },
+    { title: adminPages.menu, route: adminRoutes.menu },
+    { title: adminPages.employees, route: adminRoutes.employees },
+    { title: adminPages.purchases, route: adminRoutes.purchases },
+    { title: adminPages.reports, route: adminRoutes.reports },
+    { title: adminPages.branches, route: adminRoutes.branches },
+];
+
+export const departments = [
+    'San Salvador',
+    'Morazán',
+    'Cabañas',
+    'San Miguel',
+    'Chalatenango',
+    'La Libertad',
+    'Cuscatlán',
+    'La Unión',
+    'Ahuachapán',
+    'Sonsonate',
+    'Santa Ana',
+    'San Vicente',
+    'Usulután',
+    'La Paz'
+];
+  
