fb7d30522c5759f49bd421d034d9f950a25ba201
Carolina <00008119@uca.edu.sv>
2022-05-22 19:40:06 -0600

    Update sale card
    The sale card was changed to be able to redirect to the modify page of
    the selected sale.
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

--- Client/n-pupas/components/cards/sale.jsx
+++ Client/n-pupas/components/cards/sale.jsx
@@ -1,36 +1,39 @@
-import { crudActionTypes } from "constants/strings";
-import CrudButton from "components/buttons/crud";
+import { crudActionTypes } from 'constants/strings';
+import CrudButton from 'components/buttons/crud';
+import { adminRoutes } from 'routes/routes';
+import { useRouter } from 'next/router';
 
 const SaleCard = ({ history }) => {
-    const handleOnUpdate = () => {
-        alert("Próximamente se va a poder modificar");
-    }
+  const router = useRouter();
 
-    const handleOnDelete = () => {
-        alert("Próximamente se va a poder eliminar");
-    }
+  const handleOnUpdate = () => {
+    router.push(`${adminRoutes.editSale}/${history.id}`);
+  };
 
-    return (
-        <article className="bg-white shadow-md p-4">
-            <div className="flex flex-col mb-2  ">
-                <h1 className="text-gray-500 font-bold text-xl">Venta ID</h1>
-                <p> {` ${history.date}`} </p>
+  const handleOnDelete = () => {
+    alert('Próximamente se va a poder eliminar');
+  };
 
-                <div className="font-bold flex flex-auto justify-between ">
-                <p>{history.name}</p>
-                <p>{`$${history.price}`}</p>
-                </div>
-                <div className="font-bold flex flex-auto justify-between text-primary-500 ">
-                <p >Total </p>
-                <p>{` $${history.total}`} </p>
-                </div>
-                <div>
-                    <CrudButton actionType={crudActionTypes.update} onClickHandler={handleOnUpdate} />
-                    <CrudButton actionType={crudActionTypes.delete} onClickHandler={handleOnDelete} />
-                </div>
-            </div>
-        </article>
-    );
+  return (
+    <article className='bg-white shadow-md p-4'>
+      <div className='flex flex-col mb-2  '>
+        <h1 className='text-gray-500 font-bold text-xl'>Venta ID</h1>
+        <p>{` ${history.date}`}</p>
+        <div className='font-bold flex flex-auto justify-between mt-4'>
+          <p>{history.name} x{history.amount}</p>
+          <p>{`$${history.price}`}</p>
+        </div>
+        <div className='font-bold flex flex-auto justify-between text-primary-500 '>
+          <p className='mt-2'>Total</p>
+          <p>{` $${history.total}`} </p>
+        </div>
+        <div>
+          <CrudButton actionType={crudActionTypes.update} onClickHandler={handleOnUpdate} />
+          <CrudButton actionType={crudActionTypes.delete} onClickHandler={handleOnDelete} />
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default SaleCard;
