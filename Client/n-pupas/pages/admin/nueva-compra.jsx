bd64f3722712075bbdd6a66e09bad1549ca4d31f
Carolina <00008119@uca.edu.sv>
2022-05-28 22:32:30 -0600

    Add page to register a purchase
    Add page that allows the administrator user to register a new purchase.
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

---
+++ Client/n-pupas/pages/admin/nueva-compra.jsx
@@ -0,0 +1,20 @@
+import AddPurchaseForm from 'components/forms/add-purchase';
+import { adminPages } from 'constants/strings';
+import Head from 'next/head';
+import React from 'react';
+
+export default function NewPurchasePage() {
+  const onSubmitForm = data => {
+    alert(data.concept);
+  };
+
+  return (
+    <main className='p-6 flex flex-col gap-5'>
+      <Head>
+        <title>{adminPages.newPurchase}</title>
+      </Head>
+      <h1 className='font-bold text-2xl sm:text-3xl'>{adminPages.newPurchase}</h1>
+      <AddPurchaseForm onSubmitHandler={onSubmitForm} />
+    </main>
+  );
+}
