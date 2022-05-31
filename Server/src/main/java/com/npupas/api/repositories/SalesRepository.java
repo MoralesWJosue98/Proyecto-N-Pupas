1a7e3586b3a33fc65d7994446b409dd1ca41a6fc
MoralesWJosue98 <wilfredo.josue@live.com>
2022-05-31 17:14:16 -0600

    Add: Sales Repository

---
+++ Server/src/main/java/com/npupas/api/repositories/SalesRepository.java
@@ -0,0 +1,17 @@
+package com.npupas.api.repositories;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.npupas.api.models.entities.Branch;
+import com.npupas.api.models.entities.Sale;
+
+public interface SalesRepository extends JpaRepository<Sale, Long> {
+
+	List<Sale> findByBranch(Branch branch);
+	
+	List<Sale> findBySaleDate(LocalDate saleDate);
+
+}
