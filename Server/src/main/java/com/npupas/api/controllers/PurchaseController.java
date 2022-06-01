f6192b96af5e4fc1a130dd9a81e9dfa46359bd3b
MoralesWJosue98 <wilfredo.josue@live.com>
2022-05-31 18:53:48 -0600

    Fix: Purchase Controller

--- Server/src/main/java/com/npupas/api/controllers/PurchaseController.java
+++ Server/src/main/java/com/npupas/api/controllers/PurchaseController.java
@@ -20,71 +20,72 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/pupuserias/branches")
 public class PurchaseController {
-  @Autowired
-  PurchaseService purchaseService;
+	@Autowired
+	PurchaseService purchaseService;
 
-  @Autowired
-  BranchService branchService;
+	@Autowired
+	BranchService branchService;
 
-  @GetMapping("/{id}/purchases")
-  public ResponseEntity<List<Purchase>> getAll(@PathVariable("id") Long branchId) {
-    List<Purchase> purchases = purchaseService.getAllBranchPurchases(branchId);
-    return new ResponseEntity<List<Purchase>>(purchases, HttpStatus.OK);
-  }
+	@GetMapping("/{id}/purchases")
+	public ResponseEntity<List<Purchase>> getAll(@PathVariable("id") Long branchId) {
+		List<Purchase> purchases = purchaseService.getAllBranchPurchases(branchId);
+		return new ResponseEntity<List<Purchase>>(purchases, HttpStatus.OK);
+	}
 
-  @GetMapping("/{id}/purchases/{id_purchase}")
-  public ResponseEntity<Purchase> getOne(@PathVariable("id_purchase") Long purchaseId) {
-    Purchase foundPurchase = purchaseService.getOnePurchase(purchaseId);
-    return new ResponseEntity<Purchase>(foundPurchase, HttpStatus.OK);
-  }
+	@GetMapping("/{id}/purchases/{id_purchase}")
+	public ResponseEntity<Purchase> getOne(@PathVariable("id_purchase") Long purchaseId) {
+		Purchase foundPurchase = purchaseService.getOnePurchase(purchaseId);
+		return new ResponseEntity<Purchase>(foundPurchase, HttpStatus.OK);
+	}
 
-  @PostMapping("/{id}/purchases")
-  public ResponseEntity<MessageDTO> saveOne(@Valid AddPurchaseDTO purchaseDTO, @PathVariable("id") Long branchId,
-      BindingResult result) {
-    try {
-      if (result.hasErrors()) {
-        new ResponseEntity<MessageDTO>(
-            new MessageDTO("No se pudo guardar la compra" + result.getAllErrors().toString()),
-            HttpStatus.BAD_REQUEST);
-      }
+	@PostMapping("/{id}/purchases")
+	public ResponseEntity<MessageDTO> saveOne(@Valid AddPurchaseDTO purchaseDTO, @PathVariable("id") Long branchId,
+			BindingResult result) {
+		try {
+			if (result.hasErrors()) {
+				new ResponseEntity<MessageDTO>(
+						new MessageDTO("No se pudo guardar la compra" + result.getAllErrors().toString()),
+						HttpStatus.BAD_REQUEST);
+			}
 
-      purchaseService.save(branchId, purchaseDTO);
-      return new ResponseEntity<MessageDTO>(new MessageDTO("Compra guardada"), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<MessageDTO>(new MessageDTO("Error interno."), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+			purchaseService.save(branchId, purchaseDTO);
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Compra guardada"), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Error interno."), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-  @DeleteMapping("/{id}/purchases/{id_purchase}")
-  public ResponseEntity<MessageDTO> deleteOne(@PathVariable("id_purchase") Long purchaseId) {
-    try {
-      purchaseService.delete(purchaseId);
-      return new ResponseEntity<MessageDTO>(new MessageDTO("Compra eliminada"), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<MessageDTO>(new MessageDTO("Error interno."), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+	@DeleteMapping("/{id}/purchases/{id_purchase}")
+	public ResponseEntity<MessageDTO> deleteOne(@PathVariable("id_purchase") Long purchaseId) {
+		try {
+			purchaseService.delete(purchaseId);
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Compra eliminada"), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Error interno."), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-  @PutMapping("/{id}/purchases/{id_purchase}")
-  public ResponseEntity<MessageDTO> updateOne(@Valid AddPurchaseDTO purchaseDTO,
-      @PathVariable("id_purchase") Long purchaseId,
-      BindingResult result) {
-    try {
-      Purchase purchaseToUpdate = purchaseService.getOnePurchase(purchaseId);
+	@PutMapping("/{id}/purchases/{id_purchase}")
+	public ResponseEntity<MessageDTO> updateOne(@Valid AddPurchaseDTO purchaseDTO,
+			@PathVariable("id_purchase") Long purchaseId, BindingResult result) {
+		try {
+			Purchase purchaseToUpdate = purchaseService.getOnePurchase(purchaseId);
 
-      if (purchaseToUpdate == null || result.hasErrors()) {
-        new ResponseEntity<MessageDTO>(
-            new MessageDTO("No se pudo modificar la compra" + result.getAllErrors().toString()),
-            HttpStatus.BAD_REQUEST);
-      }
+			if (purchaseToUpdate == null || result.hasErrors()) {
+				new ResponseEntity<MessageDTO>(
+						new MessageDTO("No se pudo modificar la compra" + result.getAllErrors().toString()),
+						HttpStatus.BAD_REQUEST);
+			}
 
-      purchaseService.update(purchaseToUpdate, purchaseDTO);
-      return new ResponseEntity<MessageDTO>(new MessageDTO("Compra actualizada"), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<MessageDTO>(new MessageDTO("Error interno."), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+			purchaseService.update(purchaseToUpdate, purchaseDTO);
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Compra actualizada"), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Error interno."), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
