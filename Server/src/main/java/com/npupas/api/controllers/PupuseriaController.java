0687d696d91023bf4e5fb5b807a358938ebb6b6d
MoralesWJosue98 <wilfredo.josue@live.com>
2022-05-31 18:53:11 -0600

    Fix: Pupuseria Controller

--- Server/src/main/java/com/npupas/api/controllers/PupuseriaController.java
+++ Server/src/main/java/com/npupas/api/controllers/PupuseriaController.java
@@ -5,12 +5,14 @@ import com.npupas.api.models.entities.Pupuseria;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/pupuserias")
 public class PupuseriaController {
-    @GetMapping("/me")
-    public ResponseEntity<Pupuseria> getPupuseria(Long id) {
-        // TODO
-        return null;
-    }
+	@GetMapping("/me")
+	public ResponseEntity<Pupuseria> getPupuseria(Long id) {
+		// TODO
+		return null;
+	}
 }
