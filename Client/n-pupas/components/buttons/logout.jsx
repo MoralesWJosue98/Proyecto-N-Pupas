c7d68a3861a1ec35bbddfc98e5dd83fb608c15a7
Carolina <00008119@uca.edu.sv>
2022-06-05 18:12:53 -0600

    Add logout button component
    Add React component for the logout button to be used on the navbar, so
    the user has more accesibility.
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

---
+++ Client/n-pupas/components/buttons/logout.jsx
@@ -0,0 +1,50 @@
+import { CustomModal } from 'components/layout/modal/custom-modal';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import { confirmAlert } from 'react-confirm-alert';
+import { loginRoute } from 'routes/routes';
+import { useRouter } from 'next/router';
+
+const LogoutButton = () => {
+  const router = useRouter();
+
+  const logout = () => {
+    // Lógica para cerrar sesión
+    router.push(loginRoute);
+  };
+
+  const handleOnClick = () => {
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <CustomModal
+            onClose={onClose}
+            onConfirm={logout}
+            text={`¿Segura/o que quieres cerrar sesión?`}
+            confirmText='Confirmar'
+          />
+        );
+      },
+    });
+  };
+
+  return (
+    <button type='button' onClick={handleOnClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        class='h-6 w-6 hover:text-red-700 hover:transform hover:scale-110'
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke='currentColor'
+        stroke-width='2'
+      >
+        <path
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1'
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default LogoutButton;
