18a2d159c52f8d5a2c1ec7d8650a9b097e5c1b6f
NNieto25 <00199019@uca.edu.sv>
2022-06-10 20:49:57 -0600

    Added menu controller The menu controller was added to manage all the CRUD operations for the products.
    Authored by: Nestor Nieto <00199019@uca.edu.sv>

--- Server/src/main/java/com/npupas/api/controllers/MenuController.java
+++ Server/src/main/java/com/npupas/api/controllers/MenuController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,44 +13,86 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.npupas.api.models.dtos.AddProductDTO;
 import com.npupas.api.models.dtos.MessageDTO;
 import com.npupas.api.models.entities.Product;
+import com.npupas.api.services.MenuService;
 
 @RestController
 @RequestMapping("/pupuserias")
 public class MenuController {
 	
+	@Autowired
+	MenuService menuService;
+	
 	@GetMapping("/{id}/menu")
 	public ResponseEntity<List<Product>> getPupuseriaMenu(@PathVariable("id") Long id){
-		//TODO
-		return null;
+		try {
+			List<Product> products = menuService.getAllProducts(id);
+			return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
-	@PostMapping("/{id}/menu")
-	public ResponseEntity<MessageDTO> createProduct(@PathVariable("id") Long id, @Valid AddProductDTO product, BindingResult result){
-		//TODO
-		return null;
+	@PostMapping("/menu")
+	public ResponseEntity<MessageDTO> createProduct(@RequestHeader("Authorization") String token, @Valid AddProductDTO product, BindingResult result){
+		try {
+			if(result.hasErrors()) {
+				return new ResponseEntity<MessageDTO>(new MessageDTO("Datos de la petición son incorrectos."), HttpStatus.BAD_REQUEST);
+			}
+			
+			if(menuService.createProduct(token, product)) return new ResponseEntity<MessageDTO>(new MessageDTO("Producto creado con éxito."), HttpStatus.CREATED);
+			
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Ocurrió un problema con la petición."), HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
-	@GetMapping("/{id}/menu/{product_id}")
-	public ResponseEntity<Product> getPupuseriaMenuProduct(@PathVariable("id") Long id, @PathVariable("product_id") Long productId){
-		//TODO
-		return null;
+	@GetMapping("/menu/{id}")
+	public ResponseEntity<Product> getPupuseriaMenuProduct(@PathVariable("id") Long id, @RequestHeader("Authorization") String tokenStr){
+		try {
+			Product product = menuService.getOneProduct(tokenStr, id);
+			if(product == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+			else return new ResponseEntity<Product>(product, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
-	@PutMapping("/{id}/menu/{product_id}")
-	public ResponseEntity<Product> putPupuseriaMenuProduct(@PathVariable("id") Long id, @PathVariable("product_id") Long productId, @Valid AddProductDTO productDTO, BindingResult result ){
-		//TODO
-		return null;
+	@PutMapping("/menu/{product_id}")
+	public ResponseEntity<MessageDTO> putPupuseriaMenuProduct(@RequestHeader("Authorization") String tokenStr, @PathVariable("product_id") Long productId, @Valid AddProductDTO productDTO, BindingResult result ){
+		try {
+			if(result.hasErrors()) {
+				return new ResponseEntity<MessageDTO>(new MessageDTO("Datos de la petición son incorrectos."), HttpStatus.BAD_REQUEST);
+			}
+			
+			if(menuService.updateProduct(tokenStr, productId, productDTO)) return new ResponseEntity<MessageDTO>(new MessageDTO("Producto modificado con éxito."), HttpStatus.CREATED);
+			
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Ocurrió un problema con la petición."), HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
-	@DeleteMapping("/{id}/menu/{product_id}")
-	public ResponseEntity<Product> deletePupuseriaMenuProduct(@PathVariable("id") Long id, @PathVariable("product_id") Long productId){
-		//TODO
-		return null;
+	@DeleteMapping("/menu/{product_id}")
+	public ResponseEntity<MessageDTO> deletePupuseriaMenuProduct(@RequestHeader("Authorization") String tokenStr,  @PathVariable("product_id") Long productId){
+		try {
+			if(menuService.deleteProduct(tokenStr, productId)) return new ResponseEntity<MessageDTO>(new MessageDTO("Producto borrado con éxito."), HttpStatus.OK);
+			else return new ResponseEntity<MessageDTO>(new MessageDTO("Petición mal realizada."), HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<MessageDTO>(new MessageDTO("Error en servidor."), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
