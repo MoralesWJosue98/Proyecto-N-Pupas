9185181ae03f7b6989d51a178f1590c3585456e2
NNieto25 <00199019@uca.edu.sv>
2022-06-11 15:49:32 -0600

    Fixed the sales service The sales service was changed to make the operations in sales.
    Authored By: Nestor Nieto<00199019@uca.edu.sv>

--- Server/src/main/java/com/npupas/api/services/SalesService.java
+++ Server/src/main/java/com/npupas/api/services/SalesService.java
@@ -3,15 +3,22 @@ package com.npupas.api.services;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.npupas.api.models.dtos.AddSaleDTO;
 import com.npupas.api.models.entities.Branch;
 import com.npupas.api.models.entities.Sale;
 
 public interface SalesService {
 
-	List<Sale> getBranchSales(Branch branch);
+	List<Sale> getAllSales(Long branchId);
 
-	List<Sale> getTodaySales(LocalDate date);
+	List<Sale> getTodaySales(Long branchId);
 
-	Sale getSale(Long idSale);
+	Sale getSale(Long branchId, Long idSale);
+	
+	Boolean createSale(Long branchId, AddSaleDTO dto);
+	
+	Boolean updateSale(Long branchId, AddSaleDTO dto, Long saleId);
+	
+	Boolean deleteSale(Long branchId, Long saleId);
 
 }
--- Server/src/main/java/com/npupas/api/services/implementations/SalesServiceImpl.java
+++ Server/src/main/java/com/npupas/api/services/implementations/SalesServiceImpl.java
@@ -1,13 +1,25 @@
 package com.npupas.api.services.implementations;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.npupas.api.models.dtos.AddSaleDTO;
+import com.npupas.api.models.dtos.SalesDetailsDTO;
 import com.npupas.api.models.entities.Branch;
+import com.npupas.api.models.entities.Mass;
+import com.npupas.api.models.entities.Product;
 import com.npupas.api.models.entities.Sale;
+import com.npupas.api.models.entities.SalesDetail;
+import com.npupas.api.models.entities.SalesDetailMass;
+import com.npupas.api.repositories.BranchRepository;
+import com.npupas.api.repositories.MassRepository;
+import com.npupas.api.repositories.ProductRepository;
+import com.npupas.api.repositories.SalesDetailsMassRepository;
+import com.npupas.api.repositories.SalesDetailsRepository;
 import com.npupas.api.repositories.SalesRepository;
 import com.npupas.api.services.SalesService;
 
@@ -17,25 +29,161 @@ public class SalesServiceImpl implements SalesService {
 	@Autowired
 	SalesRepository salesRepository;
 
-	@Override
-	public List<Sale> getBranchSales(Branch branch) {
-		List<Sale> foundSale = salesRepository.findByBranch(branch);
+	@Autowired
+	BranchRepository branchRepository;
 
-		return foundSale;
+	@Autowired
+	ProductRepository productRepository;
+
+	@Autowired
+	SalesDetailsMassRepository detailsMassRepository;
+
+	@Autowired
+	MassRepository massRepository;
+
+	@Autowired
+	SalesDetailsRepository detailsRepository;
+
+	@Override
+	public List<Sale> getAllSales(Long branchId) {
+		Branch branch = branchRepository.findById(branchId).orElse(null);
+		if (branch == null)
+			return null;
+
+		return branch.getSales();
 	}
 
 	@Override
-	public List<Sale> getTodaySales(LocalDate date) {
-		List<Sale> foundTodaySale = salesRepository.findBySaleDate(date);
+	public List<Sale> getTodaySales(Long branchId) {
+		Branch branch = branchRepository.findById(branchId).orElse(null);
+		if (branch == null)
+			return null;
 
-		return foundTodaySale;
+		List<Sale> sales = salesRepository.findBySaleDateAndBranch(LocalDate.now(), branch);
+
+		return sales;
 	}
 
 	@Override
-	public Sale getSale(Long idSale) {
-		Sale foundSale = salesRepository.findById(idSale).orElse(null);
+	public Sale getSale(Long branchId, Long idSale) {
+		Branch branch = branchRepository.findById(branchId).orElse(null);
+		if (branch == null)
+			return null;
 
-		return foundSale;
+		List<Sale> sales = branch.getSales();
+		Sale sale = null;
+		if (sales.size() > 0) {
+			sales = sales.stream().filter(s -> s.getID() == idSale).toList();
+			if (sales.size() > 0)
+				sale = sales.get(0);
+		}
+
+		return sale;
+	}
+
+	@Override
+	public Boolean createSale(Long branchId, AddSaleDTO dto) {
+		Branch branch = branchRepository.findById(branchId).orElse(null);
+		if (branch == null)
+			return false;
+
+		Sale sale = new Sale();
+
+		List<SalesDetail> details = new ArrayList<SalesDetail>();
+		for (SalesDetailsDTO dt : dto.getDetails()) {
+			SalesDetail detail = new SalesDetail();
+			detail.setAmount(dt.getAmount());
+			detail.setTotal(dt.getTotal());
+			detail.setSale(sale);
+
+			Product product = productRepository.findById(dt.getIdProducto()).orElse(null);
+			if (product.getType().getType().equals("Pupusas")) {
+
+				SalesDetailMass detailsMass = new SalesDetailMass();
+				Mass mass = massRepository.findOneByMass(dt.getMass());
+				System.out.println("Lo que encontre es: " + dt.getMass());
+				detailsMass.setMass(mass);
+				detailsMass.setDetails(detail);
+
+				detail.setMassDetails(detailsMass);
+			}
+
+			detail.setProduct(product);
+			details.add(detail);
+		}
+
+		sale.setBranch(branch);
+		sale.setSaleDate(dto.getDate());
+		sale.setDetails(details);
+
+		salesRepository.save(sale);
+		return true;
+	}
+
+	@Override
+	public Boolean updateSale(Long branchId, AddSaleDTO dto, Long saleId) {
+		Branch branch = branchRepository.findById(branchId).orElse(null);
+		if(branch == null) return false;
+		
+		Sale sale = salesRepository.findById(saleId).orElse(null);
+		if(sale == null) return false;
+		
+		if(sale.getDetails().size() > 0) {
+			List<SalesDetail> allDetails = sale.getDetails();
+			sale.setDetails(null);
+			detailsRepository.deleteAll(allDetails);
+			salesRepository.save(sale);
+		}
+		
+		
+		List<SalesDetail> details = new ArrayList<SalesDetail>();
+		for(SalesDetailsDTO dt : dto.getDetails()) {
+			SalesDetail detail = new SalesDetail();
+			detail.setAmount(dt.getAmount());
+			detail.setTotal(dt.getTotal());
+			detail.setSale(sale);
+			
+			Product product = productRepository.findById(dt.getIdProducto()).orElse(null);
+			if(product.getType().getType().equals("Pupusas")) {
+				
+				SalesDetailMass detailsMass = new SalesDetailMass();
+				Mass mass = massRepository.findOneByMass(dt.getMass());
+				detailsMass.setMass(mass);
+				detailsMass.setDetails(detail);
+				
+				detail.setMassDetails(detailsMass);
+			}
+			
+			detail.setProduct(product);
+			details.add(detail);
+		}
+		
+		sale.setBranch(branch);
+		sale.setSaleDate(dto.getDate());
+		sale.setDetails(details);
+		
+		salesRepository.save(sale);
+		return true;
+	}
+
+	@Override
+	public Boolean deleteSale(Long branchId, Long saleId) {
+		Branch branch = branchRepository.findById(branchId).orElse(null);
+		if (branch == null)
+			return null;
+
+		List<Sale> sales = branch.getSales();
+		Sale sale = null;
+		if (sales.size() > 0) {
+			sales = sales.stream().filter(s -> s.getID() == saleId).toList();
+			if (sales.size() > 0)
+				sale = sales.get(0);
+		}
+		if (sale == null)
+			return false;
+
+		salesRepository.delete(sale);
+		return true;
 	}
 
 }
