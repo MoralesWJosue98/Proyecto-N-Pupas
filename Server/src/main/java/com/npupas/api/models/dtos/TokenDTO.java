16fb82ef4895214ef08b2b0f7213a6948d882e69
NNieto25 <00199019@uca.edu.sv>
2022-06-11 20:53:06 -0600

    Added role attribute on login Added the role in the login process to recognize the role on the front end.
    Authored By: Nestor Nieto <00199019@uca.edu.sv>

--- Server/src/main/java/com/npupas/api/models/dtos/TokenDTO.java
+++ Server/src/main/java/com/npupas/api/models/dtos/TokenDTO.java
@@ -2,6 +2,8 @@ package com.npupas.api.models.dtos;
 
 public class TokenDTO {
 	private String token;
+	
+	private String role;
 
 	public String getToken() {
 		return token;
@@ -15,10 +17,23 @@ public class TokenDTO {
 		super();
 		this.token = token;
 	}
+	
+
+	public TokenDTO(String token, String role) {
+		super();
+		this.token = token;
+		this.role = role;
+	}
 
 	public TokenDTO() {
 		super();
 	}
-	
-	
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
