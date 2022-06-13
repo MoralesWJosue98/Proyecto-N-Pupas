1200c5cb8a6399b1a142d53b29f9b63512f819a3
Carolina <00008119@uca.edu.sv>
2022-06-13 14:02:00 -0600

    Add get all branches functionality
    Implementation of get all branches method from PupuseriaApi.
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

--- Client/n-pupas/pages/admin/index.jsx
+++ Client/n-pupas/pages/admin/index.jsx
@@ -1,47 +1,62 @@
 import BranchSelect from 'components/forms/branchSelect';
+import { PupuseriaApi } from 'services/PupuseriaApi';
 import { homePageName } from 'constants/strings';
+import { tokenCookie } from 'constants/data';
 import HomeMenu from 'components/menu/menu';
-import useAppContext from 'context/context';
-import { branches } from 'data/tempObjects';
+import useBranchContext from 'context/BranchContext';
+import { getCookie } from 'cookies-next';
 import { useEffect } from 'react';
 import Head from 'next/head';
 
+const pupuseriaApi = new PupuseriaApi();
+
 const AdminHomePage = ({ branches }) => {
-    const { branchID, setBranchID } = useAppContext();
+  const { branchID, setBranchID } = useBranchContext();
 
-    useEffect(() => {
-        if (!branchID) {
-            setBranchID(branches[0].id);
-        }
-    }, [branches, branchID, setBranchID]);
+  useEffect(() => {
+    if (!branchID) {
+      setBranchID(branches[0].id);
+    }
+  }, [branches, branchID, setBranchID]);
 
-    const changeBranch = id => {
-        setBranchID(id);
-    };
+  const changeBranch = id => {
+    setBranchID(id);
+  };
 
-    const testPupuseriaName = 'La bendición';
+  const testPupuseriaName = 'La bendición';
 
-    return (
-        <main className='p-6 flex flex-col gap-5'>
-            <Head>
-                <title>{homePageName}</title>
-            </Head>
-            <h1 className='font-bold text-2xl sm:text-3xl'>{homePageName}</h1>
-            <section className='mb-4'>
-                <h2 className='text-primary-500 font-bold text-lg mb-3'>{`Pupusería ${testPupuseriaName}`}</h2>
-                <BranchSelect onChangeHandler={changeBranch} branches={branches} value={branchID} />
-            </section>
-            <HomeMenu isAdmin={true} />
-        </main>
-    );
+  return (
+    <main className='p-6 flex flex-col gap-5'>
+      <Head>
+        <title>{homePageName}</title>
+      </Head>
+      <h1 className='font-bold text-2xl sm:text-3xl'>{homePageName}</h1>
+      <section className='mb-4'>
+        <h2 className='text-primary-500 font-bold text-lg mb-3'>{`Pupusería ${testPupuseriaName}`}</h2>
+        <BranchSelect onChangeHandler={changeBranch} branches={branches} value={branchID} />
+      </section>
+      <HomeMenu isAdmin={true} />
+    </main>
+  );
 };
 
 export default AdminHomePage;
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ req, res }) {
+  const token = getCookie(tokenCookie, { req, res });
+
+  try {
+    const allBranches = await pupuseriaApi.getAllBranches(token);
     return {
-        props: {
-            branches: branches,
-        },
+      props: {
+        branches: allBranches,
+      },
     };
+  } catch (e) {
+    return {
+      redirect: {
+        destination: '/500',
+      },
+    };
+  }
 }
--- Client/n-pupas/pages/admin/sucursales.jsx
+++ Client/n-pupas/pages/admin/sucursales.jsx
@@ -1,15 +1,19 @@
 import { CustomModal } from 'components/layout/modal/custom-modal';
 import PageHeading from 'components/information/page-heading';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import { PupuseriaApi } from 'services/PupuseriaApi';
 import { confirmAlert } from 'react-confirm-alert';
 import BranchCard from 'components/cards/branch';
 import { adminPages } from 'constants/strings';
-import { testBranch } from 'data/tempObjects';
+import { tokenCookie } from 'constants/data';
 import { adminRoutes } from 'routes/routes';
+import { getCookie } from 'cookies-next';
 import toast from 'react-hot-toast';
 import Head from 'next/head';
 
-const BranchesPage = () => {
+const pupuseriaApi = new PupuseriaApi();
+
+const BranchesPage = ({ branches }) => {
   const deleteBranch = () => {
     // Lógica para eliminar
     toast.success('Sucursal eliminada exitosamente');
@@ -36,7 +40,7 @@ const BranchesPage = () => {
       </Head>
       <PageHeading title={adminPages.branches} route={adminRoutes.newBranch} />
       <div className='flex flex-col gap-5 md:grid md:grid-cols-2'>
-        {testBranch.map(branch => {
+        {branches.map(branch => {
           return (
             <BranchCard
               branch={branch}
@@ -51,3 +55,22 @@ const BranchesPage = () => {
 };
 
 export default BranchesPage;
+
+export async function getServerSideProps({ req, res }) {
+  const token = getCookie(tokenCookie, { req, res });
+
+  try {
+    const allBranches = await pupuseriaApi.getAllBranches(token);
+    return {
+      props: {
+        branches: allBranches,
+      },
+    };
+  } catch (e) {
+    return {
+      redirect: {
+        destination: '/500',
+      },
+    };
+  }
+}
