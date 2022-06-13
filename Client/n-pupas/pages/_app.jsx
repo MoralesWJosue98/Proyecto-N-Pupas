204a9f4a76263b3062622b1ec5e2248967a3d5b1
Carolina <00008119@uca.edu.sv>
2022-06-13 00:52:45 -0600

    Add context to handle authorization
    Add React cotext to handle methods related to authorization.
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

--- Client/n-pupas/pages/_app.jsx
+++ Client/n-pupas/pages/_app.jsx
@@ -1,3 +1,4 @@
+import { AuthContextProvider } from 'context/AuthContext';
 import { AppContextProvider } from 'context/context';
 import Layout from 'components/layout/layout';
 import { loginRoute } from 'routes/routes';
@@ -9,27 +10,29 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <AppContextProvider>
-      {router.pathname === loginRoute ? (
-        <Component {...pageProps} />
-      ) : (
-        <Layout>
+    <AuthContextProvider>
+      <AppContextProvider>
+        {router.pathname === loginRoute ? (
           <Component {...pageProps} />
-        </Layout>
-      )}
-      <Toaster
-        position='top-center'
-        reverseOrder={false}
-        containerClassName='toast-container font-bold'
-        toastOptions={{
-          style: {
-            padding: '14px',
-            background: 'rgba(0, 0, 0, 0.7)',
-            color: '#fff',
-          },
-        }}
-      />
-    </AppContextProvider>
+        ) : (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        )}
+        <Toaster
+          position='top-center'
+          reverseOrder={false}
+          containerClassName='toast-container font-bold'
+          toastOptions={{
+            style: {
+              padding: '14px',
+              background: 'rgba(0, 0, 0, 0.7)',
+              color: '#fff',
+            },
+          }}
+        />
+      </AppContextProvider>
+    </AuthContextProvider>
   );
 }
 
