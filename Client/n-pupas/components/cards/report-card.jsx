3e001648694c5d1a6144e1f8ed8c4bbf75b9042c
carolinesierra <00091119@uca.edu.sv>
2022-06-13 01:22:55 -0600

    Add report-card component
    Add report card component which is used to view employee detail report
    
    Authored by Caroline Sierra <00091119@uca.edu.sv>

---
+++ Client/n-pupas/components/cards/report-card.jsx
@@ -0,0 +1,33 @@
+import { crudActionTypes } from 'constants/strings';
+import CrudButton from 'components/buttons/crud';
+import { adminRoutes } from 'routes/routes';
+import { useRouter } from 'next/router';
+
+const ReportECard = ({  comment, onDeleteHandler }) => {
+  const router = useRouter();
+
+
+const handleOnModify = () => {
+    router.push(`${adminRoutes.editReport}/${comment.id}`);
+  };
+
+  const handleOnDelete = () => {
+    onDeleteHandler();
+  };
+
+    return (
+      <article className='bg-white shadow-md p-4'>
+        <div className="flex flex-col sm:flex-row sm:justify-between mb-2">
+          <h2 className='font-bold text-primary-500'>{comment.name}</h2>
+          <p className='font-light text-gray-700'>{comment.date}</p>
+        </div>
+        <p>{comment.body}</p>
+        <div>
+        <CrudButton actionType={crudActionTypes.update} onClickHandler={handleOnModify} />
+        <CrudButton actionType={crudActionTypes.delete} onClickHandler={handleOnDelete} />
+      </div>
+      </article>
+    );
+  };
+  
+  export default ReportECard;
