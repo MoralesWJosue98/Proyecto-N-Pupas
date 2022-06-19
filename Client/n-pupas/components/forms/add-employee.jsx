761d95a0a7d14a8723f95fa0c5aefe3af02e8b52
carolinesierra <00091119@uca.edu.sv>
2022-06-18 21:28:16 -0600

    Modified form variables
    Modified form variables to be able to create employee
    
    Authored by Caroline Sierra <00091119@uca.edu.sv>

--- Client/n-pupas/components/forms/add-employee.jsx
+++ Client/n-pupas/components/forms/add-employee.jsx
@@ -1,11 +1,10 @@
 import SectionTitle from 'components/information/section-title';
 import PrimaryButton from 'components/buttons/primary';
-import { branches } from 'data/tempObjects';
 import { titles } from 'constants/strings';
 import { useForm } from 'react-hook-form';
-import { useRef } from 'react';
+import { useRef} from 'react';
 
-const AddEmployeeForm = ({ onSubmitHandler, id, employee = false }) => {
+const AddEmployeeForm = ({ onSubmitHandler, employee = false }) => {
   const {
     register,
     handleSubmit,
@@ -13,14 +12,16 @@ const AddEmployeeForm = ({ onSubmitHandler, id, employee = false }) => {
     watch,
     reset,
   } = useForm();
-
+  
   const password = useRef({});
-  password.current = watch('password', '');
+  password.current = watch('Password', '');
 
   const onSubmit = data => {
     onSubmitHandler(data);
-    reset();
   };
+
+
+
 
   return (
     <form
@@ -34,10 +35,13 @@ const AddEmployeeForm = ({ onSubmitHandler, id, employee = false }) => {
             <input
               type='text'
               placeholder='Nombre'
-              defaultValue={employee ? employee.name : ''}
               className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
               {...register('name', {
                 required: 'Nombre requerido',
+                minLength: {
+                  value: 8,
+                  message: 'El mínimo de caracteres es 8',
+                },
                 maxLength: {
                   value: 80,
                   message: 'El máximo de caracteres es 80',
@@ -46,22 +50,41 @@ const AddEmployeeForm = ({ onSubmitHandler, id, employee = false }) => {
             />
             {errors.name && <p className='mt-1 text-red-700'>{errors.name.message}</p>}
           </div>
+        
+          <div>
+            <input
+              type='text'
+              placeholder='Nombre de usuario'
+              className= 'shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
+              {...register('userName', {
+                required: 'Nombre de usuario requerido',
+                minLength: {
+                  value: 10,
+                  message: 'El mínimo de caracteres es 10',
+                },
+                pattern: {
+                  value: /^[A-Za-z0-9_-]{10,20}$/,
+                  message: 'Algunos caracteres son inválidos',
+                },
+              })}
+            />
+            {errors.userName && <p className='mt-1 text-red-700'>{errors.userName.message}</p>}
+          </div>
+
           <div>
             <input
               type='text'
               placeholder='Fecha de contratación'
-              defaultValue={employee ? employee.hiring : ''}
               onFocus={e => (e.target.type = 'date')}
               className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
               {...register('date', { required: 'Fecha requerida' })}
             />
-            {errors.date && <p className='mt-1 text-red-700'>{errors.date.message}</p>}
+            {errors.hiringDate && <p className='mt-1 text-red-700'>{errors.hiringDate.message}</p>}
           </div>
           <div>
             <input
               type='number'
               placeholder='Salario'
-              defaultValue={employee ? employee.salary : ''}
               className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
               {...register('salary', {
                 required: 'Salario requerido',
@@ -70,60 +93,45 @@ const AddEmployeeForm = ({ onSubmitHandler, id, employee = false }) => {
             />
             {errors.salary && <p className='mt-1 text-red-700'>{errors.salary.message}</p>}
           </div>
-          <div>
-            <select
-              className='shadow border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
-              {...register('branch', { required: 'Sucursal requerida' })}
-            >
-              {branches.map(branch => {
-                return (
-                  <option key={branch.id} value={branch.id}>
-                    {branch.name}
-                  </option>
-                );
-              })}
-            </select>
-            {errors.branch && <p className='mt-1 text-red-700'>{errors.branch.message}</p>}
-          </div>
         </div>
       </section>
       <section className='flex flex-col gap-4'>
         <SectionTitle title={titles.accountData} />
-        <p className='text-center font-bold text-xl'>ID #{id}</p>
         <div className='flex flex-col gap-5 md:grid md:grid-cols-2 mb-5'>
-          <div>
-            <input
-              type='password'
-              placeholder='Contraseña'
-              className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
-              {...register('password', {
-                required: 'Contraseña requerida',
-                minLength: {
-                  value: 6,
-                  message: 'La contraseña debe tener un mínimo de 6 caracteres',
-                },
-              })}
-            />
-            {errors.password && <p className='mt-1 text-red-700'>{errors.password.message}</p>}
-          </div>
-          <div>
-            <input
-              type='password'
-              placeholder='Repetir contraseña'
-              className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
-              {...register('password_repeat', {
-                validate: value => value === password.current || 'Las contraseñas no coinciden',
-              })}
-            />
-            {errors.password_repeat && (
-              <p className='mt-1 text-red-700'>{errors.password_repeat.message}</p>
-            )}
-          </div>
+        <div>
+              <input
+                type='password'
+                placeholder='Contraseña'
+                className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
+                {...register('Password', {
+                  required: 'Contraseña requerida',
+                  minLength: {
+                    value: 6,
+                    message: 'La contraseña debe tener un mínimo de 6 caracteres',
+                  },
+                })}
+              />
+              {errors.password && <p className='mt-1 text-red-700'>{errors.password.message}</p>}
+            </div>
+            <div>
+              <input
+                type='password'
+                placeholder='Repetir contraseña'
+                className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-2 focus:border-secondary-500'
+                {...register('password_repeat', {
+                  validate: value => value === password.current || 'Las contraseñas no coinciden',
+                })}
+              />
+              {errors.password_repeat && (
+                <p className='mt-1 text-red-700'>{errors.password_repeat.message}</p>
+              )}
+            </div>
         </div>
       </section>
       <PrimaryButton text={employee ? 'Guardar' : 'Agregar'} />
     </form>
   );
-};
 
+
+};
 export default AddEmployeeForm;
