32d449d68450c4a0538946a8a19a4ecaf670b9d5
Carolina Carranza <00008119@uca.edu.sv>
2022-06-24 12:55:54 -0600

    Fix navbar to use state
    Add role state to change its routed when the role changes.
    
    Authored by Carolina Carranza <00008119@uca.edu.sv>

--- Client/n-pupas/components/layout/navbar/navbar.jsx
+++ Client/n-pupas/components/layout/navbar/navbar.jsx
@@ -1,20 +1,23 @@
-import { homePageName, profilePageName } from 'constants/strings';
 import { adminRoutes, employeeRoutes } from 'routes/routes';
 import { loginRoute, registerRoute } from 'routes/routes';
+import { adminNav, employeeNav } from 'constants/data';
 import LogoutButton from 'components/buttons/logout';
+import useAuthContext from 'context/AuthContext';
+import { useState, useEffect } from 'react';
+import { adminRole } from 'constants/data';
 import { useRouter } from 'next/router';
 import { Navlink } from './navlink';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const navigation = [
-  // TODO: Las páginas serían por rol
-  { name: `${homePageName}`, route: `${adminRoutes.home}` },
-  { name: `${profilePageName}`, route: `${adminRoutes.profile}` },
-];
-
 const Navbar = () => {
   const router = useRouter();
+  const { role } = useAuthContext();
+  const [navigation, setNavigation] = useState();
+
+  useEffect(() => {
+    setNavigation(role === adminRole ? adminNav : employeeNav);
+  }, [role]);
 
   return (
     <nav className='bg-[#FAFAFA] sticky top-0 z-10 px-2 shadow-md'>
@@ -33,13 +36,13 @@ const Navbar = () => {
       ) : (
         <div className='w-full relative flex items-center justify-between h-16 px-4 sm:px-6'>
           <div className='flex items-center cursor-pointer'>
-            <Link href={`${employeeRoutes.home}`} passHref>
+            <Link href={`${role === adminRole ? adminRoutes.home : employeeRoutes.home}`} passHref>
               <Image src='/n-pupas.png' alt='N Pupas' width={40} height={40} className='md:w-12' />
             </Link>
           </div>
           <div className='w-full ml-4 sm:ml-6 flex justify-between items-center'>
             <div className='flex space-x-6'>
-              {navigation.map(item => (
+              {navigation?.map(item => (
                 <Navlink key={item.route} name={item.name} route={item.route} />
               ))}
             </div>
