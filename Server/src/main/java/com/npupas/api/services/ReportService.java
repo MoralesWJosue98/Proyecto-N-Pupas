1f7247dc15be2d47893c6fdd2403478271dcf4fe
carolinesierra <00091119@uca.edu.sv>
2022-06-25 01:15:16 -0600

    Modified modifier function declaration
    Authored by Caroline Sierra <00091119@uca.edu.sv>

--- Server/src/main/java/com/npupas/api/services/ReportService.java
+++ Server/src/main/java/com/npupas/api/services/ReportService.java
@@ -3,6 +3,7 @@ package com.npupas.api.services;
 import java.util.List;
 
 import com.npupas.api.models.entities.Admin;
+import com.npupas.api.models.entities.Purchase;
 import com.npupas.api.models.entities.Report;
 import com.npupas.api.models.dtos.AddReportDTO;
 
@@ -10,12 +11,12 @@ public interface ReportService {
 
 	Report getOneReport(Long idReport);
 	
-	List<Report> getAllBranchReport(Long branchId);
-	
 	void save(Admin admin, Long employeeId, AddReportDTO report);
 
-	void delete(Long reportId);
-
+	void delete( Long reportId);
+	
 	Boolean update(Long reportId, AddReportDTO reportDTO);
+
+	List<Report> getAllReport(Long adminId);
 	
 }
